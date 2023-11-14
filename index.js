c29031dce8b3e8c1a4f1b1fe8ea0e0e8a9de12b0
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-14 18:05:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, returnLastTwoDrivers
+]
+
+
+function createFareMultiplier(fare){
+  return (integer) => fare * integer
+
+}
+
+const fareDoubler = (integer) => integer * 2
+const fareTripler = (integer) => integer * 3
+
+function selectDifferentDrivers(array, func){
+return func(array)
+}
